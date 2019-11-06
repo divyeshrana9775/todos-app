41adbe4a3aa4f0646bb0767d0b4dbce292711789
divyeshrana9775 <divyeshrana9775@gmail.com>
2019-11-05 21:42:53 -0500

    Updates

---
+++ precache-manifest.fe8454caf9bd89bf75bedf574f3f2fc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584779180954a1f2649521933d143c7a",
+    "url": "/todos-app/index.html"
+  },
+  {
+    "revision": "98695c7268602595a93e",
+    "url": "/todos-app/static/css/main.52de0ab1.chunk.css"
+  },
+  {
+    "revision": "a2ece9c6b960f106da77",
+    "url": "/todos-app/static/js/2.2a236271.chunk.js"
+  },
+  {
+    "revision": "98695c7268602595a93e",
+    "url": "/todos-app/static/js/main.5fd85a22.chunk.js"
+  },
+  {
+    "revision": "146ec016909c94367984",
+    "url": "/todos-app/static/js/runtime-main.5e615868.js"
+  }
+]);
